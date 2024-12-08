7d3f9572bc03cf0a755efd4cd206c04ffea9a5f1
karenzhao35 <karenzhao35@gmail.com>
2024-12-08 09:23:16 -0800

    feat: initial commit and ci/cd lint set up

---
+++ apps/web/infra/eslint.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  plugins: ["@typescript-eslint", "import", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./apps/web/web/tsconfig.json",
+    ecmaVersion: "latest",
+    allowDefaultProject: true,
+    sourceType: "module",
+    tsConfigRootDir: ".",
+    projectService: true,
+  },
+  rules: {
+    semi: ["error", "never"],
+    quotes: ["error", "double"],
+    indent: ["error", 2],
+    "comma-dangle": ["error", "only-multiline"],
+    "arrow-parens": ["error", "as-needed"],
+    "object-curly-spacing": ["error", "always"],
+    "no-restricted-imports": ["error", { patterns: ["./*", "../*"] }],
+    "import/order": [
+      "error",
+      {
+        groups: [
+          ["builtin", "external"],
+          ["internal", "parent", "sibling", "index"],
+        ],
+        "newlines-between": "always",
+        pathGroupsExcludedImportTypes: [],
+        alphabetize: { order: "asc", caseInsensitive: false },
+      },
+    ],
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+    "react-hooks/exhaustive-deps": "error",
+  },
+}
