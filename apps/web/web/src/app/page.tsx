c7c270231989a2e6ee57856ad7c48f838a2da98c
Jon Liu <jonliu4242@gmail.com>
2025-11-17 16:05:39 -0800

    fix: fixed merge errors

--- apps/web/web/src/app/page.tsx
+++ apps/web/web/src/app/page.tsx
@@ -5,12 +5,10 @@ import TotalPickups from '@/app/components/totalPickups'
 export default function Page() {
   return (
     <>
-      <TopBar />
       <h1>Home</h1>
       <div className='flex flex-col items-stretch max-w-min gap-4 m-auto'>
         <TotalPickups/>
         <TodaysPickups/>
-        <AddPickup/>
       </div>
     </>
   );
